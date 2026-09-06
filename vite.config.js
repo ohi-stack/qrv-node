27ab85e5c2b021c03ce8c8b11f4dd7aa40967642
ohi-stack <onegodianone@gmail.com>
2026-09-06 11:05:09 -0400

    Add Vite production configuration

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/platform': 'http://127.0.0.1:3000',
+      '/qr': 'http://127.0.0.1:3000',
+      '/healthz': 'http://127.0.0.1:3000',
+      '/readyz': 'http://127.0.0.1:3000',
+      '/version': 'http://127.0.0.1:3000'
+    }
+  }
+});
